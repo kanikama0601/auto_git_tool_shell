4d9c2c0456de5a1cbc3fe3a5f3bc2d87214842cd
kanikama0601 <kanikama0601school@gmail.com>
2023-05-02 09:43:17 +0900

    auto commit

--- temp/auto_git_tool.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-while :; do
-	git -C /home/kanikama0601/js1 pull
-	git -C /home/kanikama0601/js1 add .
-	git -C /home/kanikama0601/js1 commit -m "auto commit"
-	git -C /home/kanikama0601/js1 push --force
-	sudo chmod -R 777 /home/kanikama0601/js1
-	sleep 120
-done
