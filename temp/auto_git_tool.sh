80c9ed35f0459f05bcfcd6f47cdfeaf506443f4a
kanikama0601 <kanikama0601school@gmail.com>
2023-09-19 15:59:58 +0900

    auto commit

--- temp/auto_git_tool.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-while :; do
-	git -C fullpath pull
-	git -C fullpath add .
-	git -C fullpath commit -m "auto commit"
-	git -C fullpath push --force
-	chmod -R 777 fullpath
-	sleep 120
-done
